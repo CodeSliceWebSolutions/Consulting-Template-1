996b8f483995ddd80aa4b56574db1292113b1eee
CodeSliceWebSolutions <96576667+CodeSliceWebSolutions@users.noreply.github.com>
2026-01-20 12:16:48 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const reveals = document.querySelectorAll(".reveal");
+
+window.addEventListener("scroll", () => {
+    reveals.forEach(section => {
+        const top = section.getBoundingClientRect().top;
+        const trigger = window.innerHeight - 100;
+
+        if (top < trigger) {
+            section.classList.add("active");
+        }
+    });
+});
